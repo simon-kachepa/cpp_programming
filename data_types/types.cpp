77a02d5188f0c93f523921d1383f63fb8edfd3e6
simon Kachepa <kachepasimon@gmail.com>
2026-09-24 16:49:00 +0200

    Added the data types file

---
+++ data_types/types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+
+    //int - used to declare a variable as of type integer
+    int age = 30;
+    std::cout<<"Age: "<<age <<'\n';
+
+    //double - used to declare a variable as decimal number
+    double gpa = 2.1;
+    std::cout<<"GPA: "<<gpa <<'\n';
+
+    //char - used to declare a variable as a single character
+    char initial = 'S';
+    std::cout<<"Initial: "<<initial <<'\n';
+
+    //boolean used to declare a variable as either true or false
+    bool isMarried = true;
+    std::cout<<"IsMarried: "<<isMarried <<'\n';
+
+    return 0;
+}
